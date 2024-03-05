5693ae5413cc261c36587b337cf6973a1676e79a
MaishevK <kosvmail@gmail.com>
2024-03-05 21:42:31 +0300

    add a greeting to bin/brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
 
-console.log('node bin/brain-games.js');
+import readlineSync from 'readline-sync';
+
+const name = readlineSync.question('Your answer: ');
+
+console.log('brain-games');
 console.log('Welcome to the Brain Games!');
+console.log('May I have your name? ', name);
+console.log(`Hello, ${name}!`);
