35e21029a004c15f6e5dee8316bd2c6b4afd1159
MaishevK <kosvmail@gmail.com>
2024-03-06 15:32:42 +0300

    move greeting to module in cli.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,10 +1,5 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
+import ReadLine from '../src/cli.js';
 
-const name = readlineSync.question('Your answer: ');
-
-console.log('brain-games');
-console.log('Welcome to the Brain Games!');
-console.log('May I have your name? ', name);
-console.log(`Hello, ${name}!`);
+ReadLine();
