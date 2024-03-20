89839838a906b34fd55e78dbf2dd7ac58a85e9ec
MaishevK <kosvmail@gmail.com>
2024-03-20 19:40:56 +0300

    replace common functions from brain-even.js

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import readlineSync from 'readline-sync';
+import { playRound } from './games/even.js';
+
+const writeGameName = (text) => {
+    console.log(text);
+};
+  
+const writeExercise = (text) => {
+console.log(text);
+};
+
+const writeQuestion = (data) => {
+console.log('Question: ', data);
+};
+
+const writeAnswer = () => {
+    const data = readlineSync.question('Your answer: ');
+    return data;
+};
+
+const checkCorrect = (userAnswer, correctAnswer, notCorrectAnswer, name) => {
+if (userAnswer.toLowerCase() === correctAnswer) {
+    console.log('Correct!');
+    return true;
+} else if (userAnswer.toLowerCase() === notCorrectAnswer) {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
+    return false;
+}
+console.log(`'${userAnswer}' is wrong answer ;(.\nLet's try again, ${name}!`);
+return false;
+};
+
+const playThreeRounds = (name) => {
+for (let i = 0; i <= 2; i += 1) {
+    const isCorrect = playRound();
+
+    if(!isCorrect) {
+    break;
+    }
+
+    if (i === 2) {
+    console.log(`Congratulations, ${name}!`);
+    }
+}
+};
+
+  export { writeGameName, writeExercise, writeQuestion, writeAnswer, checkCorrect, playThreeRounds };
