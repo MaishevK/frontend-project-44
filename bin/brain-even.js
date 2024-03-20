38fad6a3f9728f5dc1d1df06f2b309fa09044723
MaishevK <kosvmail@gmail.com>
2024-03-20 19:31:01 +0300

    replace code with function call

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,26 +1,4 @@
 #!/usr/bin/env node
-import ReadLine from '../src/cli.js';
-import { writeGameName, writeExercise, writeQuestion, writeAnswer, checkCorrect, playThreeRounds } from '../src/games/even.js'
+import { brainEven } from '../src/games/even.js';
 
-const getNumber = () => Math.floor(Math.random() * 100);
-const isEven = (number) => !(number % 2);
-
-const playRound = () => {
-  const number = getNumber();
-  writeQuestion(number);
-  const userAnswer = writeAnswer();
-  const correctAnswer = isEven(number) ? 'yes' : 'no';
-  const notCorrectAnswer = !isEven(number) ? 'yes' : 'no';
-
-  return checkCorrect(userAnswer, correctAnswer, notCorrectAnswer, name);
-};
-
-writeGameName('brain-even\n');
-
-const name = ReadLine();
-
-writeExercise('Answer "yes" if the number is even, otherwise answer "no".');
-
-playThreeRounds(name);
-
-export { playRound };
+brainEven();
