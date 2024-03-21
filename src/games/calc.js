0e462e2d64314ba9fef2e2e9beef2f125db04a3d
MaishevK <kosvmail@gmail.com>
2024-03-22 01:00:26 +0300

    add code of game brain-calc

---
+++ src/games/calc.js
@@ -0,0 +1,52 @@
+import ReadLine from '../cli.js';
+import { writeGameName, writeExercise, writeQuestion, writeAnswer, checkCorrect, playThreeRounds } from '../index.js'
+
+let getFirstNum = 0;
+let getSecondNum = 0;
+let operatorString = '';
+let result = 0;
+let correctAnswer = 0;
+
+const getNumber = () => Math.floor(Math.random() * 100);
+
+const getOperator = () => {
+    const num = getNumber();
+    if (num <= 3) {
+        operatorString = '+';
+        result = getFirstNum + getSecondNum;
+        return ;
+    } else if (num > 6) {
+        operatorString = '*';
+        result = getFirstNum * getSecondNum;
+        return; 
+    }
+        operatorString = '-';
+        correctAnswer = getFirstNum - getSecondNum;
+        return;
+};
+
+let name = '';
+
+const playRound = () => {
+    getFirstNum = getNumber();
+    getSecondNum = getNumber();
+    getOperator();
+    writeQuestion(`${getFirstNum} ${operatorString} ${getSecondNum}`);
+    const userAnswer = Number(writeAnswer());
+    correctAnswer = result;
+    const notCorrectAnswer = userAnswer;
+
+    return checkCorrect(userAnswer, correctAnswer, notCorrectAnswer, name);
+};
+
+const brainCalc = () => {
+    writeGameName('brain-calc\n');
+
+    name = ReadLine();
+
+    writeExercise('What is the result of the expression?');
+
+    playThreeRounds(name, playRound);
+};
+
+export { brainCalc }
