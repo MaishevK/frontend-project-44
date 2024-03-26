7f59bc17147ca338dee341caf72f4523a276bbfd
MaishevK <kosvmail@gmail.com>
2024-03-26 23:47:24 +0300

    editing according to eslint rules

--- src/index.js
+++ src/index.js
@@ -1,42 +1,43 @@
 import readlineSync from 'readline-sync';
 
- 
 const writeExercise = (text) => {
-console.log(text);
+  console.log(text);
 };
 
 const writeQuestion = (data) => {
-console.log('Question: ', data);
+  console.log('Question: ', data);
 };
 
 const writeAnswer = () => {
-    const data = readlineSync.question('Your answer: ');
-    return data;
+  const data = readlineSync.question('Your answer: ');
+  return data;
 };
 
 const checkCorrect = (userAnswer, correctAnswer, notCorrectAnswer, name) => {
-if (userAnswer === correctAnswer) {
+  if (userAnswer === correctAnswer) {
     console.log('Correct!');
     return true;
-} else if (userAnswer === notCorrectAnswer) {
+  }
+  if (userAnswer === notCorrectAnswer) {
     console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
     return false;
-}
-console.log(`'${userAnswer}' is wrong answer ;(.\nLet's try again, ${name}!`);
-return false;
+  }
+  console.log(`'${userAnswer}' is wrong answer ;(.\nLet's try again, ${name}!`);
+  return false;
 };
 
 const playThreeRounds = (name, playRound) => {
-    for (let i = 0; i <= 2; i += 1) {
-        const isCorrect = playRound();
-
-        if(!isCorrect) {
-        break;
-        }
-        if (i === 2) {
-        console.log(`Congratulations, ${name}!`);
-        }
+  for (let i = 0; i <= 2; i += 1) {
+    const isCorrect = playRound();
+    if (!isCorrect) {
+      break;
     }
+    if (i === 2) {
+      console.log(`Congratulations, ${name}!`);
+    }
+  }
 };
 
-  export { writeExercise, writeQuestion, writeAnswer, checkCorrect, playThreeRounds };
+export {
+  writeExercise, writeQuestion, writeAnswer, checkCorrect, playThreeRounds,
+};
