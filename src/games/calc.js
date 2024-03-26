7bd4840b19fc907589a639ee1757c42f042b064e
MaishevK <kosvmail@gmail.com>
2024-03-26 23:44:00 +0300

    editing according to eslint rules

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,5 +1,7 @@
 import ReadLine from '../cli.js';
-import { writeExercise, writeQuestion, writeAnswer, checkCorrect, playThreeRounds } from '../index.js'
+import {
+  writeExercise, writeQuestion, writeAnswer, checkCorrect, playThreeRounds,
+} from '../index.js';
 
 let getFirstNum = 0;
 let getSecondNum = 0;
@@ -10,41 +12,43 @@ let correctAnswer = 0;
 const getNumber = () => Math.floor(Math.random() * 100);
 
 const getOperator = () => {
-    const num = getNumber();
-    if (num <= 33) {
-        operatorString = '+';
-        result = getFirstNum + getSecondNum;
-        return ;
-    } else if (num > 66) {
-        operatorString = '*';
-        result = getFirstNum * getSecondNum;
-        return; 
-    }
-        operatorString = '-';
-        result = getFirstNum - getSecondNum;
-        return;
+  const num = getNumber();
+
+  if (num <= 33) {
+    operatorString = '+';
+    result = getFirstNum + getSecondNum;
+
+    return;
+  }
+
+  if (num > 66) {
+    operatorString = '*';
+    result = getFirstNum * getSecondNum;
+    return;
+  }
+  operatorString = '-';
+  result = getFirstNum - getSecondNum;
 };
 
 let name = '';
 
 const playRound = () => {
-    getFirstNum = getNumber();
-    getSecondNum = getNumber();
-    getOperator();
-    writeQuestion(`${getFirstNum} ${operatorString} ${getSecondNum}`);
-    const userAnswer = Number(writeAnswer()) || '';
-    correctAnswer = result;
-    const notCorrectAnswer = userAnswer;
+  getFirstNum = getNumber();
+  getSecondNum = getNumber();
+  getOperator();
+  writeQuestion(`${getFirstNum} ${operatorString} ${getSecondNum}`);
+  const userAnswer = Number(writeAnswer()) || '';
+  correctAnswer = result;
+  const notCorrectAnswer = userAnswer;
 
-    return checkCorrect(userAnswer, correctAnswer, notCorrectAnswer, name);
+  return checkCorrect(userAnswer, correctAnswer, notCorrectAnswer, name);
 };
 
 const brainCalc = () => {
-    name = ReadLine();
+  name = ReadLine();
 
-    writeExercise('What is the result of the expression?');
-
-    playThreeRounds(name, playRound);
+  writeExercise('What is the result of the expression?');
+  playThreeRounds(name, playRound);
 };
 
-export { brainCalc }
+export default brainCalc;
