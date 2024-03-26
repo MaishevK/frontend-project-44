71ab37bc5decc7c5542281f177a2e2b382ed0cf6
MaishevK <kosvmail@gmail.com>
2024-03-26 23:41:34 +0300

    editing according to eslint rules

--- src/games/even.js
+++ src/games/even.js
@@ -1,26 +1,27 @@
 import ReadLine from '../cli.js';
-import { writeExercise, writeQuestion, writeAnswer, checkCorrect, playThreeRounds } from '../index.js'
+import {
+  writeExercise, writeQuestion, writeAnswer, checkCorrect, playThreeRounds,
+} from '../index.js';
 
 const getNumber = () => Math.floor(Math.random() * 100);
 const isEven = (number) => !(number % 2);
 let name = '';
 
 const playRound = () => {
-    const number = getNumber();
-    writeQuestion(number);
-    const userAnswer = writeAnswer().toLowerCase();
-    const correctAnswer = (isEven(number) ? 'yes' : 'no').toLowerCase();
-    const notCorrectAnswer = !isEven(number) ? 'yes' : 'no';
+  const number = getNumber();
+  writeQuestion(number);
+  const userAnswer = writeAnswer().toLowerCase();
+  const correctAnswer = (isEven(number) ? 'yes' : 'no').toLowerCase();
+  const notCorrectAnswer = !isEven(number) ? 'yes' : 'no';
 
-    return checkCorrect(userAnswer, correctAnswer, notCorrectAnswer, name);
+  return checkCorrect(userAnswer, correctAnswer, notCorrectAnswer, name);
 };
 
 const brainEven = () => {
-    name = ReadLine();
+  name = ReadLine();
 
-    writeExercise('Answer "yes" if the number is even, otherwise answer "no".');
-    
-    playThreeRounds(name, playRound);
-}
+  writeExercise('Answer "yes" if the number is even, otherwise answer "no".');
+  playThreeRounds(name, playRound);
+};
 
-export { brainEven };
+export default brainEven;
