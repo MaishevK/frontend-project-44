5f0443a23e48154db56e0e1fd5435f838905c2a3
MaishevK <kosvmail@gmail.com>
2024-03-28 21:57:39 +0300

    fix: to 1 is prime number

--- src/games/prime.js
+++ src/games/prime.js
@@ -12,6 +12,9 @@ const getRandomInt = (min, max) => {
 };
 
 const isPrimeNum = (num) => {
+  if (num === 1) {
+    return false;
+  }
   for (let i = 2; i <= num / 2; i += 1) {
     if (num % i === 0) {
       return false;
