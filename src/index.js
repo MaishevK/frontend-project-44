a42f5aa7cae142140ce4292144512ad7f026c3c0
MaishevK <kosvmail@gmail.com>
2024-04-02 16:19:55 +0300

    add Wellcome...Hello...

--- src/index.js
+++ src/index.js
@@ -1,43 +1,51 @@
 import readlineSync from 'readline-sync';
 
-const writeExercise = (text) => {
-  console.log(text);
-};
+// const playGame = () => {
+  const readLineInGames = () => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+    return name;
+  };
+  const writeExercise = (text) => {
+    console.log(text);
+  };
 
-const writeQuestion = (data) => {
-  console.log('Question:', data);
-};
+  const writeQuestion = (data) => {
+    console.log('Question:', data);
+  };
 
-const writeAnswer = () => {
-  const data = readlineSync.question('Your answer: ');
-  return data;
-};
+  const writeAnswer = () => {
+    const data = readlineSync.question('Your answer: ');
+    return data;
+  };
 
-const checkCorrect = (userAnswer, correctAnswer, notCorrectAnswer, name) => {
-  if (userAnswer === correctAnswer) {
-    console.log('Correct!');
-    return true;
-  }
-  if (userAnswer === notCorrectAnswer) {
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
+  const checkCorrect = (userAnswer, correctAnswer, notCorrectAnswer, name) => {
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      return true;
+    }
+    if (userAnswer === notCorrectAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
+      return false;
+    }
+    console.log(`'${userAnswer}' is wrong answer ;(.\nLet's try again, ${name}!`);
     return false;
-  }
-  console.log(`'${userAnswer}' is wrong answer ;(.\nLet's try again, ${name}!`);
-  return false;
-};
+  };
 
-const playThreeRounds = (name, playRound) => {
-  for (let i = 0; i <= 2; i += 1) {
-    const isCorrect = playRound();
-    if (!isCorrect) {
-      break;
+  const playThreeRounds = (name, playRound) => {
+    for (let i = 0; i <= 2; i += 1) {
+      const isCorrect = playRound();
+      if (!isCorrect) {
+        break;
+      }
+      if (i === 2) {
+        console.log(`Congratulations, ${name}!`);
+      }
     }
-    if (i === 2) {
-      console.log(`Congratulations, ${name}!`);
-    }
-  }
-};
-
+  };
+// };
 export {
-  writeExercise, writeQuestion, writeAnswer, checkCorrect, playThreeRounds,
+  // playGame,
+  readLineInGames, writeExercise, writeQuestion, writeAnswer, checkCorrect, playThreeRounds,
 };
