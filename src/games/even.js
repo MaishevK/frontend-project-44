86619e36c80b083d3ad28d1dfaf868eb41fd59d8
MaishevK <kosvmail@gmail.com>
2025-01-15 20:48:45 +0300

    optimisation after delete func in index.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,7 +1,6 @@
+import readlineSync from 'readline-sync';
 import ReadLine from '../cli.js';
-import {
-  writeExercise, writeQuestion, writeAnswer, checkCorrect, playThreeRounds,
-} from '../index.js';
+import { playThreeRounds, } from '../index.js';
 
 const getNumber = () => Math.floor(Math.random() * 100);
 const isEven = (number) => !(number % 2);
@@ -9,18 +8,27 @@ let name = '';
 
 const playRound = () => {
   const number = getNumber();
-  writeQuestion(number.toString());
-  const userAnswer = writeAnswer().toLowerCase();
+  console.log(number.toString());
+  const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
   const correctAnswer = (isEven(number) ? 'yes' : 'no').toLowerCase();
   const notCorrectAnswer = !isEven(number) ? 'yes' : 'no';
 
-  return checkCorrect(userAnswer, correctAnswer, notCorrectAnswer, name);
+  if (userAnswer === correctAnswer) {
+    console.log('Correct!');
+    return true;
+  }
+  if (userAnswer === notCorrectAnswer) {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
+    return false;
+  }
+  console.log(`'${userAnswer}' is wrong answer ;(.\nLet's try again, ${name}!`);
+  return false;
 };
 
 const brainEven = () => {
   name = ReadLine();
 
-  writeExercise('Answer "yes" if the number is even, otherwise answer "no".');
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
   playThreeRounds(name, playRound);
 };
 
