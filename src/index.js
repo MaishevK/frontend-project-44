b2d02e94e97706ee09b21b849d4976b1ac0f0f34
MaishevK <kosvmail@gmail.com>
2025-01-15 20:46:18 +0300

    delete functions

--- src/index.js
+++ src/index.js
@@ -1,31 +1,3 @@
-import readlineSync from 'readline-sync';
-
-const writeExercise = (text) => {
-  console.log(text);
-};
-
-const writeQuestion = (data) => {
-  console.log('Question:', data);
-};
-
-const writeAnswer = () => {
-  const data = readlineSync.question('Your answer: ');
-  return data;
-};
-
-const checkCorrect = (userAnswer, correctAnswer, notCorrectAnswer, name) => {
-  if (userAnswer === correctAnswer) {
-    console.log('Correct!');
-    return true;
-  }
-  if (userAnswer === notCorrectAnswer) {
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
-    return false;
-  }
-  console.log(`'${userAnswer}' is wrong answer ;(.\nLet's try again, ${name}!`);
-  return false;
-};
-
 const playThreeRounds = (name, playRound) => {
   for (let i = 0; i <= 2; i += 1) {
     const isCorrect = playRound();
@@ -38,6 +10,4 @@ const playThreeRounds = (name, playRound) => {
   }
 };
 
-export {
-  writeExercise, writeQuestion, writeAnswer, checkCorrect, playThreeRounds,
-};
+export { playThreeRounds, };
